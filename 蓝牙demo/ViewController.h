dc096a712bc61ea081a87975363bf9d5bb9b6ffc
Mymac <mymac@MymacdeMac-mini.local>
2015-08-07 12:06:32 +0800

    Initial Commit

---
+++ 蓝牙demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  蓝牙demo
+//
+//  Created by Mymac on 15/8/7.
+//  Copyright (c) 2015年 Mymac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 蓝牙demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  蓝牙demo
+//
+//  Created by Mymac on 15/8/7.
+//  Copyright (c) 2015年 Mymac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
